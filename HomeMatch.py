ddff60b515d6679c0d56f235a81c31f528705957
Michael Antia <michael_antia@knights.ucf.edu>
2024-10-24 17:41:11 -0500

    Added HomeMatch

---
+++ HomeMatch.py
@@ -0,0 +1,10 @@
+#A starter file for the HomeMatch application if you want to build your solution in a Python program instead of a notebook. 
+
+import os
+from dotenv import load_dotenv
+from langchain.llms import OpenAI
+
+load_dotenv()
+
+
+
